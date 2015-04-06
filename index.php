43f9d1b0d279d1f7316f6fcfcc98a51ce13c9727
joni2back <joni2back@gmail.com>
2015-04-06 16:47:55 -0300

    css styles

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html ng-app="HoleApp">
-  <head>
+
+<head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
     <title></title>
-
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
@@ -12,27 +12,27 @@
     <script src="assets/js/main.js"></script>
     <script src="assets/js/styles.map.js"></script>
     <script src="assets/js/controller.js"></script>
-
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/main.css?v=1">
+</head>
 
-  </head>
-  <body ng-controller="HoleAppCtrl">
+<body ng-controller="HoleAppCtrl">
 
-  <div class="main" id="map-canvas"></div>
-  <div class="report-nav">
-      <button class="btn btn-lg btn-material" ng-click="openModalReport()">
-        <i class="glyphicon glyphicon-plus"></i>
-      </button>
-  </div>
-  <div class="title-nav">
-    <img src="http://www.monumentoalabandera.gob.ar/images/logo_muni2012.png" alt=""/>
-  </div>
+    <div class="main" id="map-canvas">
+    </div>
+    <div class="report-nav">
+        <button class="btn btn-lg btn-material" ng-click="openModalReport()">
+            <i class="glyphicon glyphicon-plus"></i>
+        </button>
+    </div>
+    <div class="title-nav">
+        <img src="assets/img/logo.png" alt="" />
+    </div>
+    <div ng-include="'assets/templates/report-form.html'"></div>
+    <div ng-include="'assets/templates/infowindow.html'"></div>
+    <div ng-include="'assets/templates/infowindow-report.html'"></div>
 
-  <div ng-include="'assets/templates/report-form.html'"></div>
-  <div ng-include="'assets/templates/infowindow.html'"></div>
-  <div ng-include="'assets/templates/infowindow-report.html'"></div>
+</body>
 
-  </body>
 </html>
