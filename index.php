8da396e5f8e623512225d80405f48295319f2cef
joni2back <joni2back@gmail.com>
2015-04-08 09:13:18 -0300

    removed templates to prevent the canvas loading

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
-    <title></title>
+    <title>HoleApp</title>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
@@ -19,19 +19,34 @@
 
 <body ng-controller="HoleAppCtrl">
 
-    <div class="main" id="map-canvas">
-    </div>
+    <div class="main" id="map-canvas"></div>
+
     <div class="report-nav">
         <button class="btn btn-lg btn-material" ng-click="openModalReport()">
             <i class="glyphicon glyphicon-plus"></i>
         </button>
     </div>
+
     <div class="title-nav">
         <img src="assets/img/logo.png" alt="" />
     </div>
+
     <div ng-include="'assets/templates/report-form.html'"></div>
-    <div ng-include="'assets/templates/infowindow.html'"></div>
-    <div ng-include="'assets/templates/infowindow-report.html'"></div>
+
+    <div class="hide">
+        <div id="info-window">
+            <h4>{{ infoWindow.data.title }}</h4>
+            <p>{{ infoWindow.data.content }}</p>
+            <p>{{ infoWindow.data.address }} ({{ infoWindow.data.zone }})</p>
+            <p>Tamaño: {{ findHoleByValue(infoWindow.data.size).label }}</p>
+            <div ng-show="infoWindow.data.photo">
+                <img ng-src="{{ infoWindow.data.photo ? 'backend/web/uploads/' + infoWindow.data.photo : '' }}" alt="" />
+            </div>
+        </div>
+        <div id="info-window-report">
+            <a href="" ng-click="openModalReport()" tabindex="-1">Reportar aca ({{ contextMenuAddress }})</a>
+        </div>
+    </div>
 
 </body>
 
